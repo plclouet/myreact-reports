f92073567335044b1dfcf5976e22b7e0b825cea4
plclouet <plclouet@gmail.com>
2021-09-24 23:25:46 +0200

    search bar protocoles and models

--- src/models/searchBarModels.js
+++ src/models/searchBarModels.js
@@ -34,7 +34,7 @@ export default class SearchBarModels extends Component {
         { ({ handleSubmit, handleChange, handleBlur, isSubmitting }) => (
          
 
-         <div className="container-fluid p-5">
+         <div className="container-fluid p-2 ">
            <div className="row">
          
           <form  className="col s12" onSubmit={ handleSubmit }>
@@ -42,14 +42,14 @@ export default class SearchBarModels extends Component {
        
 
          
-           {/*  <div className="form-group col s12 m6">
-              <input className="form-control" name="nom_contains"  placeholder="Search ..." onChange={ handleChange } onBlur={ handleBlur } />
-            </div> */}
+           <div className="form-group col s12 m-2">
+              <input className="form-control" name="modelTitre_contains"  placeholder="Search ..." onChange={ handleChange } onBlur={ handleBlur } />
+            </div> 
        
 
 
           
-            <div className="form-group col s12 m2">
+           {/*  <div className="form-group col s12 m2">
             <select className="browser-default custom-select" name="modelTitre_contains"  onChange={ handleChange } onBlur={ handleBlur }>
               <option value="">tous</option>
               <option value="crane">crane</option>
@@ -69,11 +69,11 @@ export default class SearchBarModels extends Component {
               <option value="hanche">hanche</option>    
               <option value="genou">genou</option> 
               <option value="cheville">cheville</option> 
-              <option value="pied">pied</option>      
+              <option value="pied">pied</option>     
             </select>
-            </div>
+            </div> */}
        
-          <div className="col" style={{marginTop:0}}>
+          <div className="col s12 m-2 d-flex justify-content-end" style={{marginTop:0, padding:0}}>
             <button className="btn btn-small btn-success" type="submit" disabled={ isSubmitting } >Submit</button>
           </div> 
             
