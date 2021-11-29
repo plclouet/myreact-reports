f9c7a1a76325802310a93172b8bedfb73dae1611
plclouet <plclouet@gmail.com>
2021-11-29 09:17:25 +0100

    V19: ajout dans la timeline des boutons edit et show; changement de la barre de navigation avec ajout de la timeline

--- src/header/index.js
+++ src/header/index.js
@@ -24,8 +24,8 @@ return(
             <button className="btn btn-info m-2">Login</button>
         </Link>
         <button className="btn btn-danger m-2" onClick={() => handleLogout()}>Logout</button>
-        <Link to="/">
-            <button className="btn btn-success m-2">Accueil</button>
+        <Link to="/timeline">
+            <button className="btn btn-success m-2">Timeline</button>
         </Link>
         </div>
     </nav>
--- src/timeline/components/TimelineItem.js
+++ src/timeline/components/TimelineItem.js
@@ -1,5 +1,6 @@
 // import { render } from '@testing-library/react';
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const TimelineItem = ({ data }) => {
     
@@ -7,7 +8,7 @@ const TimelineItem = ({ data }) => {
    
   
     
-    if (isRight==="IRMosteo"){
+    if (isRight==="IRMgene"){
         return(
           
             <div className="timeline-item right">
@@ -19,15 +20,26 @@ const TimelineItem = ({ data }) => {
                 </span>
                 <time className="pb-2">{data.rdvDate.slice(0,16)}</time>
                 <p>{data.nom} {data.prenom}</p>
-                <span>{data.titre}</span>
-                
+                <div>{data.titre}</div>
+                <div className="after-none">
+                        <Link to={`/edit-report/${data.id}`}>
+                          <button className="btn btn-primary btn-sm mx-2">Edit</button>
+                        </Link>
+                     
+                    
+                        <Link to={`/show-report/${data.id}`}>
+                          <button className="btn btn-secondary btn-sm mx-auto">Show</button>
+                        </Link>
+                </div>
+               
+                   
                 <span className="circle" />
             </div>
            
         </div>
         )}else{
             return(
-                <div className="timeline-item">
+                <div className="timeline-item left">
                 <div className="timeline-item-content">
                     
                     <span className="tag" style={{ backgroundColor: "#273c75" }}>
@@ -35,8 +47,18 @@ const TimelineItem = ({ data }) => {
                     </span>
                     <time className="pb-2">{data.rdvDate.slice(0,16)}</time>
                     <p>{data.nom} {data.prenom}</p>
-                    <span>{data.titre}</span>
                     
+                    <div className="after-none">
+                            <Link to={`/edit-report/${data.id}`}>
+                            <button className="btn btn-primary btn-sm mx-2">Edit</button>
+                            </Link>
+                        
+                        
+                            <Link to={`/show-report/${data.id}`}>
+                            <button className="btn btn-secondary btn-sm mx-auto">Show</button>
+                            </Link>
+                    </div>
+                    <div>{data.titre}</div>
                     <span className="circle" />
                 </div>
             </div>
