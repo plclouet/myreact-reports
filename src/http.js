0aab90bf00822ba4da4c87f71ff058d9ba6cd739
plclouet <plclouet@gmail.com>
2022-04-09 13:18:13 +0200

    V21: retrait link fontawesome dans index.html et suppression console log jwtToken

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const jwtToken = localStorage.getItem("JWT_REPORTS");
-console.log(jwtToken);
+//console.log(jwtToken);
 // export const API_URL = "http://localhost:1337";
 export const API_URL = "https://salty-ravine-75314.herokuapp.com";
 
